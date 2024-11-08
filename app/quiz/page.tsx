47fbe8e20167447cf6313ce8b658fc525ed076ec
GearHead87 <hosanulislam87@gmail.com>
2024-11-08 19:59:02 +0600

    quiz page added

---
+++ app/quiz/page.tsx
@@ -0,0 +1,22 @@
+'use client';
+import useQuiz from '@/hooks/useQuiz';
+import { QuizType } from '@/lib/type';
+import QuizInterface from './components/quiz-interface';
+
+const QuizPage = () => {
+	const { data, isLoading } = useQuiz();
+
+	const quizQuestions = data as unknown as QuizType[];
+
+	if (isLoading) {
+		return <div>Loading...</div>;
+	}
+
+	return (
+		<div className="px-4 py-8">
+			<QuizInterface quizQuestions={quizQuestions} />
+		</div>
+	);
+};
+
+export default QuizPage;
